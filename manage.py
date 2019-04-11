65dec8d2ab5750d81963cffe7b708f0e3621afeb
Edward Jose <edward@me.com>
2019-04-11 17:04:05 +0800

    manage.py code adjustments

--- manage.py
+++ manage.py
@@ -10,13 +10,10 @@ from webapp import blueprint
 
 app = create_app(os.getenv('BOILERPLATE_ENV') or 'dev')
 app.register_blueprint(blueprint)
-
 app.app_context().push()
 
-manager = Manager(app)
-
 migrate = Migrate(app, db)
-
+manager = Manager(app)
 manager.add_command('db', MigrateCommand)
 
 @manager.command
